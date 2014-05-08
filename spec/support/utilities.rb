78ab4c381cadb9dc1d0dbccc0280dc006a2b02b7
Riaz Virani <riaz.n.virani@gmail.com>
2014-05-07 22:20:28 -0400

    tests chap 5

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,4 @@
+=begin
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -6,3 +7,6 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+=end
+
+include ApplicationHelper
