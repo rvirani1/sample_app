879b4aa92bf5b7655ce84b7efcbfc1dc52403e6a
Riaz Virani <riaz.n.virani@gmail.com>
2014-07-08 14:20:22 -0400

    Finished Listing 9.46

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -45,6 +45,18 @@ describe "Authentication" do
 
   describe "authorization" do
 
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
     describe "when attempting to visit a protected page" do
       let(:user) { FactoryGirl.create(:user) }
       before do
