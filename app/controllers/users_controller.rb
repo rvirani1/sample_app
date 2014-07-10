30c814618129a2d0d337a14e23777b14777afd68
Riaz Virani <riaz.n.virani@gmail.com>
2014-07-09 20:01:38 -0400

    finished chap 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,25 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    if signed_in?
+      redirect_to root_url
+    else
+      @user = User.new
+    end
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    if signed_in?
+      redirect_to root_url
     else
-      render 'new'
+      @user = User.new(user_params)
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
 
@@ -40,7 +48,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    @destroyUser = User.find(params[:id])
+    @destroyUser.destroy unless (@destroyUser == @user)
     flash[:success] = "User deleted."
     redirect_to users_url
   end
